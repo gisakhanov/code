3a6e787471fe01d6250a9a4b576deef1ec935942
George Isakhanov <magogi44@yahoo.com>
2020-07-20 13:43:04 -0400

    Create DataTranformationException.java

---
+++ com/availity/DataTranformationException.java
@@ -0,0 +1,36 @@
+package com.availity;
+
+public class DataTranformationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6800003240136673963L;
+
+	public DataTranformationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataTranformationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataTranformationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataTranformationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DataTranformationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
